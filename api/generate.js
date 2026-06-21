f5dc81b8f46ff37d0d733ce56fa9bfde72d07ff6
PlanetOffic <adityaoffc0@gmail.com>
2026-06-22 00:35:27 +0800

    Refactor CORS headers and simplify OPTIONS handling

--- api/generate.js
+++ api/generate.js
@@ -1,17 +1,9 @@
 export default async function handler(req, res) {
-  // connection function 
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  // fuck request
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' });
-  }
+  if (req.method === 'OPTIONS') return res.status(200).end();
 
   const randomPart = Math.random().toString(36).substring(2, 10) + 
                      Math.random().toString(36).substring(2, 10);
