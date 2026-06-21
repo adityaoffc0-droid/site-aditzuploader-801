0600ebdbb5ade2d04ead8fc442ea0f92d6785a19
PlanetOffic <adityaoffc0@gmail.com>
2026-06-22 00:19:03 +0800

    Remove commented API key from generate.js
    Removed commented-out API key from the handler function.

--- api/generate.js
+++ api/generate.js
@@ -1,8 +1,7 @@
 export default async function handler(req, res) {
-  // api key js 6767
   const randomPart = Math.random().toString(36).substring(2, 10) + 
                      Math.random().toString(36).substring(2, 10);
-  
+
   const apiKey = `adtz-${randomPart}`;
 
   res.status(200).json({ apiKey: apiKey });
